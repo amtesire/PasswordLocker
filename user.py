b58a10f586caafeddf9bcf78740f199c200eee66
amtesire <tesiregisele@gmail.com>
2020-11-17 11:41:44 +0200

    added save info

--- user.py
+++ user.py
@@ -16,3 +16,8 @@ class User:
         self.last_name = last_name
         self.user_name = user_name
         self.password = password
+    def save_user(self):
+        '''
+        save_user method saves user objects into user_list
+        '''
+        User.user_list.append(self)    
