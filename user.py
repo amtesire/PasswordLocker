2685cc5b6e3be9a30520eff865fec3e6afa36c48
amtesire <tesiregisele@gmail.com>
2020-11-17 11:40:34 +0200

    added user info

--- user.py
+++ user.py
@@ -3,3 +3,16 @@ class User:
     Class that generates new user instances
     """
     user_list = []
+    def __init__(self,first_name, last_name, user_name, password):
+        '''
+        __init__ method that helps us define properties for our objects.
+        Args:
+            first_name: New user first name.
+            last_name : New user last name.
+            user_name: New user username.
+            password : New user password.
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.password = password
